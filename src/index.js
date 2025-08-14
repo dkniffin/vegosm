0928049ba534e01c7a8cda781536b6a5c855014d
Derek Kniffin <derek@quimbee.com>
2025-08-13 22:51:51 -0400

    Use vegan icon

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import "leaflet/dist/leaflet.css"
 import "leaflet-overpass-layer"
 import "leaflet-overpass-layer/dist/OverPassLayer.css"
 import "./css"
+import veganIconUrl from "./icons/vegan.png"
 
 document.addEventListener("DOMContentLoaded", () => {
   var map = L.map('map').setView([51.505, -0.09], 15);
@@ -12,9 +13,14 @@ document.addEventListener("DOMContentLoaded", () => {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
   }).addTo(map);
 
+  const veganIcon = L.icon({
+      iconUrl: veganIconUrl
+    })
+
   var opl = new L.OverPassLayer({
     "query": 'nwr["diet:vegan"]({{bbox}});out geom;',
-    minZoom: 10
+    minZoom: 10,
+    markerIcon: veganIcon
   })
 
   map.addLayer(opl)
