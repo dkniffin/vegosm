62bc4a9bb73fc63871332fec8c76b64c972f9363
Derek Kniffin <derek@quimbee.com>
2025-08-14 12:22:45 -0400

    Use vegan icon + marker

--- src/index.js
+++ src/index.js
@@ -1,26 +1,32 @@
 import "leaflet"
 import "leaflet/dist/leaflet.css"
+
 import "leaflet-overpass-layer"
 import "leaflet-overpass-layer/dist/OverPassLayer.css"
+
+import "leaflet.icon.glyph"
+
 import "./css"
-import veganIconUrl from "./icons/vegan.png"
 
 document.addEventListener("DOMContentLoaded", () => {
-  var map = L.map('map').setView([51.505, -0.09], 15);
+  var map = L.map('map').setView([51.505, -0.10], 15);
 
   L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
   }).addTo(map);
 
-  const veganIcon = L.icon({
-      iconUrl: veganIconUrl
-    })
+  // TODO: change marker color
+  const veganMarker = L.icon.glyph({
+		prefix: "icon",
+		glyph: "vegan",
+    glyphColor: "white"
+	})
 
   var opl = new L.OverPassLayer({
     "query": 'nwr["diet:vegan"]({{bbox}});out geom;',
     minZoom: 10,
-    markerIcon: veganIcon
+    markerIcon: veganMarker
   })
 
   map.addLayer(opl)
