aa4dd3eae96ddc4003d09d7161f170403115dce4
Derek Kniffin <derek@quimbee.com>
2025-08-13 22:39:44 -0400

    Show veg options on map

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 import "leaflet"
-import "leaflet/dist/leaflet.css";
-// import "leaflet-overpass-layer"
+import "leaflet/dist/leaflet.css"
+import "leaflet-overpass-layer"
+import "leaflet-overpass-layer/dist/OverPassLayer.css"
 import "./css"
 
 document.addEventListener("DOMContentLoaded", () => {
-  var map = L.map('map').setView([51.505, -0.09], 13);
+  var map = L.map('map').setView([51.505, -0.09], 15);
 
   L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
   }).addTo(map);
 
-  // var opl = new L.OverPassLayer({
-  //   "query": "(node({{bbox}})[organic];node({{bbox}})[second_hand];);out qt;",
-  // })
+  var opl = new L.OverPassLayer({
+    "query": 'nwr["diet:vegan"]({{bbox}});out geom;',
+    minZoom: 10
+  })
 
-  // map.addLayer(opl)
+  map.addLayer(opl)
 })
